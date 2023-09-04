05ab33e5b2a6c37740a7cc2ef0ccf1f19fe33e59
jaken92 <jaaakeeen@gmail.com>
2023-09-04 14:53:03 +0200

    Dynamic ports + minor readme update
    Changed the portnumber in frontend/index.js to be dynamic. Updated the readme setup instructions to correlate with the changeds.

--- README.md
+++ README.md
@@ -5,7 +5,8 @@ A simple mixer of a class of students into groups of two (and one group of three
 **NOTE** This is mainly thought of as an assignment for my students at the [Web developer education, YRGO, Gothenburg](https://www.yrgo.se/program/webbutvecklare). I appreciate your interest in this project, but allow me to focus more on their future this time.
 
 ## Installation
-The application consist of a backend part and a frontend. The backend would prefarably run on port 4000 (otherwise, change value on **line 6** in [frontend/index.js](frontend/index.js))
+
+The application consist of a backend part and a frontend. The backend would prefarably run on port 4000 (otherwise, change value of backPort in [frontend/index.js](frontend/index.js))
 
 ```bash
 cd backend
@@ -24,4 +25,5 @@ php -S localhost:8080
 The backend depends on a sqlite database, containing a single table (WU22). If you want to change this value, you have to edit [backend/index.php](backend/index.php) at line 8 and 10. Note that the sqlite file is not included in this package, as it could contain personal data. You will need to setup your own sqlite3 file. It should contain a single table, and that table should have the columns id (int), lastname (string) and firstname (string)
 
 ## Usage
+
 Now, visit localhost:8080 in your browser and the top-left container should be filled with names. Drag and drop the ones that aren't present to the container below. When done, click "set pair-programmers", and pairings of the present students should emerge to the right.
--- frontend/index.js
+++ frontend/index.js
@@ -1,9 +1,12 @@
-let present = document.getElementById("present");
-let away = document.getElementById("away");
-let result = document.getElementById("result");
+//set the backPort value on the row below to the portnumber you want use to start your localhost on.
+const backPort = '4000';
+
+let present = document.getElementById('present');
+let away = document.getElementById('away');
+let result = document.getElementById('result');
 
 const getStudents = async () => {
-  const response = await fetch("http://localhost:4000/index.php");
+  const response = await fetch('http://localhost:' + backPort + '/index.php');
   const classList = await response.json();
   renderList(classList);
 };
@@ -13,20 +16,20 @@ let classList = getStudents();
 
 const renderList = (list) => {
   list.forEach((person) => {
-    let box = document.createElement("div");
-    box.classList.add("student");
-    box.setAttribute("draggable", true);
-    box.addEventListener("dragstart", dragStart);
+    let box = document.createElement('div');
+    box.classList.add('student');
+    box.setAttribute('draggable', true);
+    box.addEventListener('dragstart', dragStart);
     box.innerText = person.firstname;
     box.id = person.id;
     present.appendChild(box);
   });
 };
 
-away.addEventListener("dragenter", dragEnter);
-away.addEventListener("dragover", dragOver);
-away.addEventListener("dragleave", dragLeave);
-away.addEventListener("drop", drop);
+away.addEventListener('dragenter', dragEnter);
+away.addEventListener('dragover', dragOver);
+away.addEventListener('dragleave', dragLeave);
+away.addEventListener('drop', drop);
 
 // Drag and drop
 function allowDrop(ev) {
@@ -34,31 +37,31 @@ function allowDrop(ev) {
 }
 
 function dragStart(e) {
-  e.dataTransfer.setData("text/plain", e.target.id);
+  e.dataTransfer.setData('text/plain', e.target.id);
   setTimeout(() => {
-    e.target.classList.add("hide");
+    e.target.classList.add('hide');
   }, 0);
 }
 
 function dragEnter(e) {
   e.preventDefault();
-  e.target.classList.add("drag-over");
+  e.target.classList.add('drag-over');
 }
 
 function dragOver(e) {
   e.preventDefault();
-  e.target.classList.add("drag-over");
+  e.target.classList.add('drag-over');
 }
 
 function dragLeave(e) {
-  e.target.classList.remove("drag-over");
+  e.target.classList.remove('drag-over');
 }
 
 function drop(e) {
-  e.target.classList.remove("drag-over");
+  e.target.classList.remove('drag-over');
 
   // get the draggable element
-  const id = e.dataTransfer.getData("text/plain");
+  const id = e.dataTransfer.getData('text/plain');
   console.log(id);
   const draggable = document.getElementById(id);
 
@@ -66,27 +69,27 @@ function drop(e) {
   e.target.appendChild(draggable);
 
   // display the draggable element
-  draggable.classList.remove("hide");
+  draggable.classList.remove('hide');
 }
 
 // Show students
 function shuffleStudents() {
   let students = present.childNodes;
-  let thirds = students.length %2 !== 0 ? true : false
+  let thirds = students.length % 2 !== 0 ? true : false;
   for (let i = students.length; i > 0; i--) {
     result.appendChild(students[(Math.random() * i) | 0]);
   }
-  if (thirds){
+  if (thirds) {
     addThirdsClass();
   }
-  function addThirdsClass () {
-      let x = result.childNodes.length
-      let i = 1
-      for (const student of result.childNodes) {
-        if (i >= x - 2) {
-          student.classList.add('thirds');
-        }
-        i++
+  function addThirdsClass() {
+    let x = result.childNodes.length;
+    let i = 1;
+    for (const student of result.childNodes) {
+      if (i >= x - 2) {
+        student.classList.add('thirds');
       }
+      i++;
     }
+  }
 }
