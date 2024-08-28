b8dd3a79797d274ecc977b6a640491b56d648a1a
Julia <jsgoransson@gmail.com>
2024-08-28 17:27:41 +0200

    changed to more descriptive names

--- backend/index.php
+++ backend/index.php
@@ -5,13 +5,11 @@ declare(strict_types=1);
 header('Content-Type: application/json');
 header('Access-Control-Allow-Origin: *');
 
-$table = 'WU22';
-$dbName = 'MagnusVV_Issue';
+$table = 'Students';
+$dbName = 'studentdb';
 $db = new PDO('sqlite:' . $dbName . '.sqlite3');
 
 $stmt = $db->query("SELECT * FROM '$table'");
 $stmt->execute();
 $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
 echo json_encode($result);
-
-var_dump($db);
